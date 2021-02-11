9f2cacaf0a1080b51337e219e0c4442ee5b86baf
LucasCross2023 <817750@mystma.org>
2021-02-11 16:52:39 +0000

    unit 3 test try 1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+    count = 0
+    for i in range(0, len(w)):
+        if w[i] == "b":
+            count = count + 1
+    
+    return count 
+
+print(countB("baseball"))
+
+def removeLast(w):
+    letter = w[-1]
+    
+    return letter
+
+print(removeLast("winter"))
+
+def firstLast(w):
+    if w[0] == w[-1]:
+        return True
+    else:
+        return False
+
+print(firstLast("alph"))
+
+def firstLast(w):
+    if w[0] == w[-1]:
+        return True
+    else:
+        return False 
+
+print(firstLast("roar"))
